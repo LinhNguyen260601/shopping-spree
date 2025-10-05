270307581a9ddeb89f7a935e6f7d21038106dc8c
LinhNguyen260601 <linhnh.260601@gmail.com>
2025-10-05 18:43:22 +0700

    Revert "chore(set up): Set up the eslint, prettier, install some pacakges are using in the project"

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import useRouteElements from '@/useRouteElements'
-
-const App = () => {
-  const routeElements = useRouteElements()
-
-  return <>{routeElements}</>
-}
-
-export default App
--- src/constants/path.ts
+++
@@ -1,7 +0,0 @@
-export const PATH = {
-  LOGIN: '/login',
-  REGISTER: '/register',
-  HOME: '/'
-} as const
-
-export type Path = (typeof PATH)[keyof typeof PATH]
